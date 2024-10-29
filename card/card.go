653fceed29a5e724399f60a10cdcb74afd46d4a1
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-29 10:41:00 -0400

    Implemented functionality to fetch cards

---
+++ card/card.go
@@ -0,0 +1,39 @@
+package card
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/stevezaluk/mtgjson-models/card"
+	"github.com/stevezaluk/mtgjson-models/errors"
+	"github.com/stevezaluk/mtgjson-sdk-client/context"
+)
+
+func GetCard(uuid string) (card.Card, error) {
+	var result card.Card
+
+	var uri = context.GetUri() + "/card" + "?cardId=" + uuid
+
+	resp, err := http.Get(uri)
+
+	if resp.StatusCode == 404 {
+		return result, errors.ErrNoCard
+	}
+
+	if resp.StatusCode == 400 {
+		return result, errors.ErrInvalidUUID
+	}
+
+	if err != nil {
+		return result, errors.ErrNoCard
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+
+	if _err := json.Unmarshal(body, &result); _err != nil {
+		return result, _err
+	}
+
+	return result, nil
+}
