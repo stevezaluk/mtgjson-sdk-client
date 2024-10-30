e8062eb3c9a2aedff9a40c252b25c58563ca2a01
Steven A. Zaluk <stevezaluk@gmail.com>
2024-10-30 14:42:16 -0400

    Implemented functionality for fetching basic deck metadata

--- deck/deck.go
+++ deck/deck.go
@@ -1,1 +1,35 @@
 package deck
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/stevezaluk/mtgjson-models/deck"
+	"github.com/stevezaluk/mtgjson-models/errors"
+	"github.com/stevezaluk/mtgjson-sdk-client/context"
+)
+
+func GetDeck(code string) (deck.Deck, error) {
+	var result deck.Deck
+
+	var uri = context.GetUri("/deck") + "?deckCode=" + code
+
+	resp, err := http.Get(uri)
+
+	if resp.StatusCode == 404 {
+		return result, errors.ErrNoDeck
+	}
+
+	if err != nil {
+		return result, err
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+
+	if _err := json.Unmarshal(body, &result); err != nil {
+		return result, _err
+	}
+
+	return result, nil
+}
