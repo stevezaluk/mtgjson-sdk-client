17ebe00eb58cf92809094a93de1389133ee89938
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-03 19:29:51 -0500

    Implemented functionality for creating new decks. Name, Code, and Type are required

--- deck/deck.go
+++ deck/deck.go
@@ -1,6 +1,7 @@
 package deck
 
 import (
+	"bytes"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -32,4 +33,56 @@ func GetDeck(code string) (deck.Deck, error) {
 	}
 
 	return result, nil
+}
+
+func NewDeck(deck deck.Deck) (bool, error) {
+	if deck.Name == "" || deck.Code == "" {
+		return false, errors.ErrDeckMissingId
+	}
+
+	var uri = context.GetUri("/deck")
+
+	deckBytes, err := json.Marshal(&deck)
+	if err != nil {
+		return false, err
+	}
+
+	resp, err := http.Post(uri, "application/json", bytes.NewBuffer(deckBytes))
+
+	if resp.StatusCode == 500 {
+		return false, errors.ErrDeckUpdateFailed
+	}
+
+	if resp.StatusCode == 400 {
+		// return ErrDeckInvalid
+	}
+
+	if resp.StatusCode == 409 {
+		return false, errors.ErrDeckAlreadyExists
+	}
+
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func CreateDeck(name string, code string, deckType string) (bool, error) {
+	var new deck.Deck
+
+	if name == "" || code == "" {
+		return false, errors.ErrDeckMissingId
+	}
+
+	new.Name = name
+	new.Code = code
+	new.Type = deckType
+
+	_, err := NewDeck(new)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
 }
