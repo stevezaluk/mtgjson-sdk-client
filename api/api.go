6ad7c340e59699fa4f601d4d401046535e8bcf02
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-31 17:44:28 -0500

    Implemented a basic struct representing the API along with a constructor function: New()

--- api/api.go
+++ api/api.go
@@ -1,1 +1,23 @@
 package api
+
+import (
+	"github.com/go-resty/resty/v2"
+)
+
+/*
+MtgjsonApi A representation of the MTGJSON API and all of its routes
+*/
+type MtgjsonApi struct {
+	client *resty.Client
+}
+
+/*
+New Initialize a new MTGJSON API object
+*/
+func New() *MtgjsonApi {
+	client := resty.New()
+
+	return &MtgjsonApi{
+		client: client,
+	}
+}
