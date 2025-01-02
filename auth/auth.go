374fd897a5a69b912093eee7569dbf2ad36565bd
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-02 14:41:21 -0500

    Implemented basic abstraction for the authentication API

--- auth/auth.go
+++ auth/auth.go
@@ -1,1 +1,21 @@
 package auth
+
+import "github.com/stevezaluk/mtgjson-sdk-client/client"
+
+/*
+AuthApi A representation of the auth namespace for the MTGJSON API
+*/
+type AuthApi struct {
+	BaseUrl string
+	client  *client.HTTPClient
+}
+
+/*
+New Create a new instance of the AuthApi struct
+*/
+func New(baseUrl string, client *client.HTTPClient) *AuthApi {
+	return &AuthApi{
+		BaseUrl: baseUrl,
+		client:  client,
+	}
+}
