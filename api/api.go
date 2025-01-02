74f82361bca253d5ab66216837ec2899ede606d8
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-02 14:31:13 -0500

    Updated MtgjsonApi struct to initialize the currently implemented endpoints

--- api/api.go
+++ api/api.go
@@ -1,19 +1,29 @@
 package api
 
-import "github.com/stevezaluk/mtgjson-sdk-client/client"
+import (
+	"github.com/stevezaluk/mtgjson-sdk-client/card"
+	"github.com/stevezaluk/mtgjson-sdk-client/client"
+	"github.com/stevezaluk/mtgjson-sdk-client/deck"
+)
 
 /*
 MtgjsonApi A representation of the MTGJSON API and all of its routes
 */
 type MtgjsonApi struct {
 	Client *client.HTTPClient
+	Deck   *deck.DeckApi
+	Card   *card.CardApi
 }
 
 /*
 New Initialize a new MTGJSON API object
 */
-func New() *MtgjsonApi {
+func New(baseUrl string) *MtgjsonApi {
+	httpClient := client.NewHttpClient()
+
 	return &MtgjsonApi{
-		Client: client.NewHttpClient(),
+		Client: httpClient,
+		Deck:   deck.New(baseUrl, httpClient),
+		Card:   card.New(baseUrl, httpClient),
 	}
 }
