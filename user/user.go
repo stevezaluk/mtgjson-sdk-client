fa9c688dadba23fbc6f87904d436400b7cc81534
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-02 18:37:11 -0500

    Implemented a basic abstraction of the user namespace

--- user/user.go
+++ user/user.go
@@ -1,2 +1,21 @@
 package user
 
+import "github.com/stevezaluk/mtgjson-sdk-client/client"
+
+/*
+UserApi A representation of the user namespace for the MTGJSON API
+*/
+type UserApi struct {
+	BaseUrl string
+	client  *client.HTTPClient
+}
+
+/*
+New Create a new instance of the UserApi struct
+*/
+func New(baseUrl string, client *client.HTTPClient) *UserApi {
+	return &UserApi{
+		BaseUrl: baseUrl,
+		client:  client,
+	}
+}
