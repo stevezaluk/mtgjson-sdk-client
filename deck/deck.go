bf4441ffb3186711d1983687290345fbaf8522d5
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-03 22:30:31 -0500

    Implemented support for fetching single decks from the API

--- deck/deck.go
+++ deck/deck.go
@@ -1,7 +1,10 @@
 package deck
 
 import (
+	deckModel "github.com/stevezaluk/mtgjson-models/deck"
+	sdkErrors "github.com/stevezaluk/mtgjson-models/errors"
 	"github.com/stevezaluk/mtgjson-sdk-client/client"
+	"net/http"
 )
 
 /*
@@ -22,4 +25,40 @@ func New(baseUrl string, client *client.HTTPClient) *DeckApi {
 		BaseUrl: baseUrl,
 		client:  client,
 	}
+}
+
+/*
+GetDeck Fetch a deck from the MongoDB database using the code passed in the parameter. Owner
+is the email address of the user that you want to assign to the deck. If the string is empty
+then it does not filter by user. Returns ErrNoDeck if the deck does not exist or cannot be located
+*/
+func (api *DeckApi) GetDeck(code string, owner string) (*deckModel.Deck, error) {
+	request := api.client.BuildRequest(&deckModel.Deck{}).
+		SetQueryParams(map[string]string{"deckCode": code, "owner": owner})
+
+	resp, err := request.Get(api.BaseUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Error() != nil {
+		if resp.StatusCode() == http.StatusUnauthorized {
+			return nil, sdkErrors.ErrTokenInvalid
+		}
+
+		if resp.StatusCode() == http.StatusForbidden {
+			return nil, sdkErrors.ErrInvalidPermissions
+		}
+
+		if resp.StatusCode() == http.StatusNotFound {
+			return nil, sdkErrors.ErrNoDeck
+		}
+
+		if resp.StatusCode() == http.StatusBadRequest { // this should never get returned
+			return nil, sdkErrors.ErrDeckMissingId
+		}
+	}
+
+	return resp.Result().(*deckModel.Deck), nil
+
 }
