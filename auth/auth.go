e659a912d3777b7c44b207e18baabf9fb2823b4a
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-03 21:04:50 -0500

    Implemented support for resetting user passwords

--- auth/auth.go
+++ auth/auth.go
@@ -1,8 +1,10 @@
 package auth
 
 import (
+	"errors"
 	"github.com/auth0/go-auth0/authentication/oauth"
 	"github.com/spf13/viper"
+	apiModels "github.com/stevezaluk/mtgjson-models/api"
 	sdkErrors "github.com/stevezaluk/mtgjson-models/errors"
 	"github.com/stevezaluk/mtgjson-sdk-client/client"
 	"net/http"
@@ -60,4 +62,86 @@ func (api *AuthApi) SetAuthToken(email string, password string) error {
 	viper.Set("api.token", tokenSet)
 
 	return nil
+}
+
+/*
+RegisterUser Register a new user with Auth0 and store there user model within the MongoDB database
+*/
+func (api *AuthApi) RegisterUser(email string, username string, password string) (*apiModels.APIResponse, error) {
+	if email == "" || username == "" || password == "" {
+		return nil, sdkErrors.ErrUserMissingId
+	}
+
+	request := api.client.BuildRequest(&apiModels.APIResponse{}).
+		SetBody(apiModels.RegisterRequest{
+			Username: username,
+			Email:    email,
+			Password: password,
+		})
+
+	resp, err := request.Post(viper.GetString("api.base_url") + "/register")
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Error() != nil {
+		errorResult := resp.Error().(*apiModels.APIResponse)
+		if resp.StatusCode() == http.StatusConflict { // this needs to be added to the API
+			return errorResult, sdkErrors.ErrUserAlreadyExist
+		}
+
+		if resp.StatusCode() == http.StatusBadRequest {
+			if errorResult.Err == sdkErrors.ErrInvalidPasswordLength.Error() {
+				return errorResult, sdkErrors.ErrInvalidPasswordLength
+			}
+
+			if errorResult.Err == sdkErrors.ErrInvalidEmail.Error() {
+				return errorResult, sdkErrors.ErrInvalidEmail
+			}
+		}
+
+		if resp.StatusCode() == http.StatusInternalServerError {
+			return errorResult, sdkErrors.ErrFailedToRegisterUser
+		}
+	}
+
+	return resp.Result().(*apiModels.APIResponse), nil
+}
+
+/*
+ResetUserPassword Send a reset password email to a specified user account
+*/
+func (api *AuthApi) ResetUserPassword(email string) (*apiModels.APIResponse, error) {
+	request := api.client.BuildRequest(&apiModels.APIResponse{}).SetQueryParam("email", email)
+
+	resp, err := request.Get(viper.GetString("api.base_url") + "/reset")
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Error() != nil {
+		errResult := resp.Error().(*apiModels.APIResponse)
+
+		if resp.StatusCode() == http.StatusUnauthorized {
+			return errResult, sdkErrors.ErrTokenInvalid
+		}
+
+		if resp.StatusCode() == http.StatusForbidden {
+			return errResult, sdkErrors.ErrInvalidPermissions
+		}
+
+		if resp.StatusCode() == http.StatusBadRequest {
+			return errResult, sdkErrors.ErrInvalidEmail
+		}
+
+		if resp.StatusCode() == http.StatusNotFound {
+			return errResult, sdkErrors.ErrNoUser
+		}
+
+		if resp.StatusCode() == http.StatusInternalServerError {
+			return errResult, errors.New("user: Failed to reset user password") // this needs to be added as a named error
+		}
+	}
+
+	return resp.Result().(*apiModels.APIResponse), nil
 }
