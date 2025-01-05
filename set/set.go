2039d0ec6adcaf81925fcea26f88b3ecd703ba70
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-05 16:17:26 -0500

    Implemented support for fetching a single set

--- set/set.go
+++ set/set.go
@@ -1,6 +1,11 @@
 package set
 
-import "github.com/stevezaluk/mtgjson-sdk-client/client"
+import (
+	sdkErrors "github.com/stevezaluk/mtgjson-models/errors"
+	setModel "github.com/stevezaluk/mtgjson-models/set"
+	"github.com/stevezaluk/mtgjson-sdk-client/client"
+	"net/http"
+)
 
 /*
 SetApi A representation of the set namespace for the MTGJSON API
@@ -18,4 +23,34 @@ func New(baseUrl string, client *client.HTTPClient) *SetApi {
 		BaseUrl: baseUrl,
 		client:  client,
 	}
+}
+
+/*
+GetSet Takes a single string representing a set code and returns a set model for the set.
+Returns ErrNoSet if the set does not exist, or cannot be located
+*/
+func (api *SetApi) GetSet(code string, owner string) (*setModel.Set, error) {
+	request := api.client.BuildRequest(&setModel.Set{}).
+		SetQueryParams(map[string]string{"setCode": code, "owner": owner})
+
+	resp, err := request.Get(api.BaseUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Error() != nil {
+		if resp.StatusCode() == http.StatusUnauthorized {
+			return nil, sdkErrors.ErrTokenInvalid
+		}
+
+		if resp.StatusCode() == http.StatusForbidden {
+			return nil, sdkErrors.ErrInvalidPermissions
+		}
+
+		if resp.StatusCode() == http.StatusNotFound {
+			return nil, sdkErrors.ErrNoSet
+		}
+	}
+
+	return resp.Result().(*setModel.Set), nil
 }
