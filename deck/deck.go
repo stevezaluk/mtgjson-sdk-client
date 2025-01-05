08bf2b67e5907186529056db6804ca75d2bf89ef
Steven A. Zaluk <stevezaluk@gmail.com>
2025-01-05 00:18:11 -0500

    Implemented support for fetching deck contents

--- deck/deck.go
+++ deck/deck.go
@@ -60,5 +60,37 @@ func (api *DeckApi) GetDeck(code string, owner string) (*deckModel.Deck, error) 
 	}
 
 	return resp.Result().(*deckModel.Deck), nil
+}
 
+/*
+GetDeckContents Update the 'contents' field of the deck passed in the parameter. This accepts a
+pointer and updates this in place to avoid having to copy large amounts of data
+*/
+func (api *DeckApi) GetDeckContents(code string, owner string) (*deckModel.DeckContents, error) {
+	request := api.client.BuildRequest(&deckModel.DeckContents{}).SetQueryParams(map[string]string{"deckCode": code, "owner": owner})
+
+	resp, err := request.Get(api.BaseUrl + "/content")
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Error() != nil {
+		if resp.StatusCode() == http.StatusUnauthorized {
+			return nil, sdkErrors.ErrTokenInvalid
+		}
+
+		if resp.StatusCode() == http.StatusForbidden {
+			return nil, sdkErrors.ErrInvalidPermissions
+		}
+
+		if resp.StatusCode() == http.StatusNotFound {
+			return nil, sdkErrors.ErrNoDeck
+		}
+
+		if resp.StatusCode() == http.StatusBadRequest {
+			return nil, sdkErrors.ErrDeckMissingId
+		}
+	}
+
+	return resp.Result().(*deckModel.DeckContents), nil
 }
