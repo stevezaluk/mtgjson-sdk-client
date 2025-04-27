2717bf569a6f56f1bfe618cc13a516d80aa35424
Steven A. Zaluk <stevezaluk@gmail.com>
2025-04-27 10:52:00 -0400

    Added comments for API namespace interface

--- api/namespace.go
+++ api/namespace.go
@@ -6,6 +6,9 @@ import "net/http"
 Namespace - An interface that all API namespaces (Card, Deck, User, etc.) implement
 */
 type Namespace interface {
+	// Client - A function that returns a pointer to the HTTP client used for the namespace
 	Client() *http.Client
+
+	// BaseURL - A function that returns the base URL used in HTTP requests
 	BaseURL() string
 }
